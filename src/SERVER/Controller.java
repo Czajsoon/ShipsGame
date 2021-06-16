f4d018916d409bc5c478b3aed296c65dce8dc6f0
Jakub Czajkowski <44880904+Czajsoon@users.noreply.github.com>
2021-06-16 23:05:28 +0200

    visible hi!

--- src/SERVER/Controller.java
+++ src/SERVER/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package SERVER;
 
 public class Controller {
 }
--- src/SERVER/Main.java
+++ src/SERVER/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package SERVER;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
